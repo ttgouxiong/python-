0ae01abff60a563d542e557c5aa7287fb40552af
ttgouxiong <1998.tt@163.com>
2022-02-08 21:38:52 +0800

    threading

--- multi_threading.py
+++ multi_threading.py
@@ -1,5 +1,6 @@
 import threading
 import time
+from queue import Queue
 
 def main():
     # 看看有几个线程
@@ -41,7 +42,31 @@ def main2():
     thread2.join()
     print('all done\n')
 
+def job(l,q):
+    for i in range(len(l)):
+        l[i] = l[i]**2
+    q.put(l)
+
+def multithreading(data):
+    q = Queue()
+    threads = []
+
+    for i in range(4):
+        t = threading.Thread(target=job, args=(data[i],q))
+        t.start()
+        threads.append(t)
+    for thread in threads:
+        thread.join()
+    results = []
+    for _ in range(4):
+        results.append(q.get())
+    print(results)
+    return results
+
+
 if __name__ == '__main__':
     # main()
     # main1()
-    main2()
+    # main2()
+    data = [[1, 2, 3], [3, 4, 5], [4, 4, 4], [5, 5, 5]]
+    multithreading(data)
